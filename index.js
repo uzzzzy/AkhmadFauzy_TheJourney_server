d2b25693f31bf63b0126aae0fb1e7bc42974f62b
uzzzzy <a.fauzy4b@gmail.com>
2021-09-22 17:41:22 +0800

    concurrently

--- index.js
+++ index.js
@@ -9,11 +9,15 @@ const app = express()
 
 const router = require(`./src/v${apiVersion}/routes`)
 
+const cors = require('cors')
+
 // run
 app.use(express.json())
 
 app.use(`/api/v${apiVersion}`, router)
 
 app.use(express.static(__dirname + '/html'))
+
+app.use(cors())
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
